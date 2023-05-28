15a2bd328a7e4c355414ad679282ab6f72422157
Tinex3 <b.riquelme.gomez@gmail.com>
2023-05-27 23:36:49 -0400

    final

--- Ejercicio 2/Riquelme_Benjamin_js.js
+++ Ejercicio 2/Riquelme_Benjamin_js.js
@@ -7,3 +7,18 @@ function myBirthYearFunc(){
 
 console.log(myBirthYearFunc());
 //Lo que deberia salir en consola seria "Naci en 1980"   
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+   }
+
+   //el console.log indicaria que "nací en x" siendo x la edad que se defina cuando se llame la funcion, en caso de no definirse saldra undefined
+
+function add(num1, num2){//num1 = 10 num2 = 20
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);//el console.log indicaria que seria 30
+
+   }
---
+++ Extra/js/prueba.js
@@ -0,0 +1,9 @@
+var num = 15;
+console.log(num);
+function logAndReturn(num2){
+ console.log(num2);
+ return num2;
+}
+var result = logAndReturn(10);
+console.log(result);
+console.log(num);
